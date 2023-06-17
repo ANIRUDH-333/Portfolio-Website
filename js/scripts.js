58fa4ef6d635926c953e068be0829522fd600bae
Anirudh Edpuganti <aniedpuganti@gmail.com>
2023-06-18 01:51:59 +0530

    updated website

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
 // Initially, there's nothing here.
 // You can add JavaScript to make your website more interactive.
+// script.js
+$(document).ready(function(){
+    $("#navbar").load("navbar.html");
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.createElement('link');
+    link.rel = 'icon';
+    
+    // Use the appropriate file type (e.g. 'image/gif' for animated GIF, 'image/png' for PNG)
+    link.type = 'image/gif';
+    link.href = 'images/icons8-3d.gif';
+    
+    document.getElementsByTagName('head')[0].appendChild(link);
+});
